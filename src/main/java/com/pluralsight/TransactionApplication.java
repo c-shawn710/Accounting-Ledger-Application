f48483c9e0c89d549b97c2924f360ccc5a6f9a5e
Shawn_Chung <c_shawn710@yahoo.com>
2024-10-15 13:12:48 -0700

    Created Ledger options: All, Deposits, Payments

--- src/main/java/com/pluralsight/TransactionApplication.java
+++ src/main/java/com/pluralsight/TransactionApplication.java
@@ -1,19 +1,20 @@
 package com.pluralsight;
 
 import java.io.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TransactionApplication {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        LocalDate today = LocalDate.now();
-        LocalTime currentTime = LocalTime.now();
-        LocalDateTime dateTime = LocalDateTime.now();
+        homePage(scanner);
+    }
 
+    public static void homePage(Scanner scanner) {
         System.out.println("Welcome to the Home screen! Please select an option: \nD) Add Deposit\nP) Make Payment\nL) View Ledger screen\nX) Exit the application");
         String homeOption;
 
@@ -28,7 +29,7 @@ public class TransactionApplication {
                 break;
 
             } else if (homeOption.equalsIgnoreCase("L")) {
-                ledger(); //create ledger
+                displayLedger(scanner); //create ledger
                 break;
 
             } else if (homeOption.equalsIgnoreCase("X")) {
@@ -40,6 +41,7 @@ public class TransactionApplication {
         }
         scanner.close();
     }
+
 
     public static void addDeposit(Scanner scanner) {
         System.out.println("Please enter deposit info\nItem Description: ");
@@ -67,9 +69,34 @@ public class TransactionApplication {
         saveInfo(payment);
     }
 
-    public static void ledger() {
+    public static void displayLedger(Scanner scanner) {
+        System.out.println("Welcome to the Ledger screen!\nSelect which option you would like to view: \nA) All entries\nD) Deposits\nP) Payments\nR) Reports\nH) Return to Home screen");
+        String ledgerOption;
 
-    }
+        while (true) {
+            ledgerOption = scanner.nextLine();
+            if (ledgerOption.equalsIgnoreCase("A")) {
+                allEntries();
+                break;
+            } else if (ledgerOption.equalsIgnoreCase("D")) {
+                deposits();
+                break;
+            } else if (ledgerOption.equalsIgnoreCase("P")) {
+                payments();
+                break;
+            } else if (ledgerOption.equalsIgnoreCase("R")) {
+                System.out.println("ily leslie");
+                //viewReports();
+                break;
+            } else if (ledgerOption.equalsIgnoreCase("H")) {
+                homePage(scanner);
+            } else {
+                System.out.println("Please select a valid option: D, P, R, H");
+
+            }
+        }
+    }       //make more methods and create ledger class
+
 
     //Save transactions to the csv file
     public static void saveInfo(Info info) {
@@ -79,6 +106,52 @@ public class TransactionApplication {
         } catch (IOException e) {
             System.out.println("Error");
             e.printStackTrace();
+        }
+    }
+
+    public static void allEntries() {
+        try {
+            String viewAll = Files.readString(Paths.get("transactions.csv"));
+            System.out.println(viewAll);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deposits() {
+        try {
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String input;
+            bufReader.readLine();
+
+            while ((input = bufReader.readLine()) != null) {
+                String[] columns = input.split("\\|");
+                if (Double.parseDouble(columns[4]) > 0) {
+                    System.out.println(input);
+                }
+            }
+            fileReader.close();
+            bufReader.close();
+        } catch (Exception e) {
+        }
+    }
+    private static void payments() {
+        try {
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String input;
+            bufReader.readLine();
+
+            while ((input = bufReader.readLine()) != null) {
+                String[] columns = input.split("\\|");
+                if (Double.parseDouble(columns[4]) < 0) {
+                    System.out.println(input);
+                }
+            }
+            fileReader.close();
+            bufReader.close();
+        } catch (Exception e) {
         }
     }
 }
