95fc2b48f4288262fe5437e70e91a6b401d21ed0
Shawn_Chung <c_shawn710@yahoo.com>
2024-10-15 18:19:58 -0700

    Finished Ledger

--- src/main/java/com/pluralsight/TransactionApplication.java
+++ src/main/java/com/pluralsight/TransactionApplication.java
@@ -3,8 +3,7 @@ package com.pluralsight;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class TransactionApplication {
@@ -35,6 +34,7 @@ public class TransactionApplication {
             } else if (homeOption.equalsIgnoreCase("X")) {
                 System.out.println("Exiting Application");
                 System.exit(0);
+                break;
             } else {
                 System.out.println("Please select a valid option: D, P, L, X");
             }
@@ -50,10 +50,12 @@ public class TransactionApplication {
         String vendorDepositInfo = scanner.nextLine();
         System.out.println("Amount: ");
         double amountDeposit = scanner.nextDouble();
-        scanner.nextLine();
+
 
         Info deposit = new Info(depositDescriptionInfo, vendorDepositInfo, amountDeposit);
         saveInfo(deposit);
+        Scanner homeScanner = new Scanner(System.in);
+        homePage(homeScanner);
     }
 
     public static void makePayment(Scanner scanner) {
@@ -63,10 +65,12 @@ public class TransactionApplication {
         String vendorPaymentInfo = scanner.nextLine();
         System.out.println("Amount (Input negative): ");
         double amountPayment = scanner.nextDouble();
-        scanner.nextLine();
 
         Info payment = new Info(paymentDescriptionInfo, vendorPaymentInfo, amountPayment);
         saveInfo(payment);
+
+        Scanner homeScanner = new Scanner(System.in);
+        homePage(homeScanner);
     }
 
     public static void displayLedger(Scanner scanner) {
@@ -85,17 +89,20 @@ public class TransactionApplication {
                 payments();
                 break;
             } else if (ledgerOption.equalsIgnoreCase("R")) {
-                System.out.println("ily leslie");
-                //viewReports();
+                reports(scanner);
                 break;
             } else if (ledgerOption.equalsIgnoreCase("H")) {
                 homePage(scanner);
             } else {
                 System.out.println("Please select a valid option: D, P, R, H");
-
             }
         }
-    }       //make more methods and create ledger class
+        //Help from friend to loop back to view options instead of app exiting
+        System.out.println("Press Enter to go back");
+        scanner.nextLine();
+        Scanner ledgerScanner = new Scanner(System.in);
+        displayLedger(ledgerScanner);
+    }
 
 
     //Save transactions to the csv file
@@ -136,6 +143,7 @@ public class TransactionApplication {
         } catch (Exception e) {
         }
     }
+
     private static void payments() {
         try {
             FileReader fileReader = new FileReader("transactions.csv");
@@ -154,4 +162,129 @@ public class TransactionApplication {
         } catch (Exception e) {
         }
     }
-}
+
+    private static void reports(Scanner scanner) {
+        System.out.println("Welcome to the Reports screen!\nSelect which option you would like to view: \n1) Month To Date\n2) Previous Month\n3) Year To Date\n4) Previous Year\n5) Search by Vendor\n0) Back to Ledger screen");
+        int reportsOption;
+
+        while (true) {
+            reportsOption = scanner.nextInt();
+            scanner.nextLine();
+            if (reportsOption == 1) {
+                monthToDate();
+                break;
+            } else if (reportsOption == 2) {
+                previousMonth();
+                break;
+            } else if (reportsOption == 3) {
+                yearToDate();
+                break;
+            } else if (reportsOption == 4) {
+                previousYear();
+                break;
+            } else if (reportsOption == 5) {
+                searchVendor(scanner);
+                break;
+            } else if (reportsOption == 0) {
+                displayLedger(scanner);
+                break;
+            } else {
+                System.out.println("Please select a valid option: 1, 2, 3, 4, 5, 0");
+            }
+        }
+    }
+
+    private static void monthToDate() {
+        try {
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String input;
+            bufReader.readLine();
+            LocalDate inputDate;
+            LocalDate currentDate = LocalDate.now();
+            while ((input = bufReader.readLine()) != null) {
+                String[] columns = input.split("\\|");
+                inputDate = LocalDate.parse(columns[0]);
+                if (inputDate.getMonthValue() == currentDate.getMonthValue() && inputDate.getYear() == currentDate.getYear()) {
+                    System.out.println(input);
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    private static void previousMonth() {
+        try {
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String input;
+            bufReader.readLine();
+            LocalDate inputDate;
+            LocalDate currentDate = LocalDate.now();
+            while ((input = bufReader.readLine()) != null) {
+                String[] columns = input.split("\\|");
+                inputDate = LocalDate.parse(columns[0]);
+                if (inputDate.getMonthValue() == currentDate.getMonthValue() - 1 && inputDate.getYear() == currentDate.getYear()) {
+                    System.out.println(input);
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    private static void yearToDate() {
+        try {
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String input;
+            bufReader.readLine();
+            LocalDate inputDate;
+            LocalDate currentDate = LocalDate.now();
+            while ((input = bufReader.readLine()) != null) {
+                String[] columns = input.split("\\|");
+                inputDate = LocalDate.parse(columns[0]);
+                if (inputDate.getYear() == currentDate.getYear()) {
+                    System.out.println(input);
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+        private static void previousYear () {
+            try {
+                FileReader fileReader = new FileReader("transactions.csv");
+                BufferedReader bufReader = new BufferedReader(fileReader);
+                String input;
+                bufReader.readLine();
+                LocalDate inputDate;
+                LocalDate currentDate = LocalDate.now();
+                while ((input = bufReader.readLine()) != null) {
+                    String[] columns = input.split("\\|");
+                    inputDate = LocalDate.parse(columns[0]);
+                    if (inputDate.getMonthValue() == currentDate.getMonthValue() && inputDate.getYear() == currentDate.getYear() - 1) {
+                        System.out.println(input);
+                    }
+                }
+            } catch (Exception e) {
+            }
+        }
+
+        private static void searchVendor (Scanner scanner) {
+            try {
+                FileReader fileReader = new FileReader("transactions.csv");
+                BufferedReader bufReader = new BufferedReader(fileReader);
+                String input;
+                bufReader.readLine();
+                System.out.println("Enter Vendor name: ");
+                String inputVendor = scanner.nextLine();
+                while ((input = bufReader.readLine()) != null) {
+                    String[] columns = input.split("\\|");
+                    String vendorName = columns[3];
+                    if(vendorName.equalsIgnoreCase(inputVendor)) {
+                        System.out.println(input);
+                    }
+                }
+            } catch (Exception e) {
+            }
+        }
+    }
